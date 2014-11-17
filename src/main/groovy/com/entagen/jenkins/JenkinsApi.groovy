acc1bbc8f927ec446ef9000da61abb1f9f40a024
Grzegorz Poznachowski <gpoznachowski@neoteric.eu>
2014-11-17 10:47:50 +0100

    replacing branch path in template config file

--- src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
@@ -12,63 +12,63 @@ import org.apache.http.protocol.HttpContext
 import org.apache.http.HttpRequest
 
 class JenkinsApi {
-    String jenkinsServerUrl
-    RESTClient restClient
-    HttpRequestInterceptor requestInterceptor
-    boolean findCrumb = true
-    def crumbInfo
+	String jenkinsServerUrl
+	RESTClient restClient
+	HttpRequestInterceptor requestInterceptor
+	boolean findCrumb = true
+	def crumbInfo
 
-    public void setJenkinsServerUrl(String jenkinsServerUrl) {
-        if (!jenkinsServerUrl.endsWith("/")) jenkinsServerUrl += "/"
-        this.jenkinsServerUrl = jenkinsServerUrl
-        this.restClient = new RESTClient(jenkinsServerUrl)
-    }
+	public void setJenkinsServerUrl(String jenkinsServerUrl) {
+		if (!jenkinsServerUrl.endsWith("/")) jenkinsServerUrl += "/"
+		this.jenkinsServerUrl = jenkinsServerUrl
+		this.restClient = new RESTClient(jenkinsServerUrl)
+	}
 
-    public void addBasicAuth(String jenkinsServerUser, String jenkinsServerPassword) {
-        println "use basic authentication"
+	public void addBasicAuth(String jenkinsServerUser, String jenkinsServerPassword) {
+		println "use basic authentication"
 
-        this.requestInterceptor = new HttpRequestInterceptor() {
-            void process(HttpRequest httpRequest, HttpContext httpContext) {
-                def auth = jenkinsServerUser + ':' + jenkinsServerPassword
-                httpRequest.addHeader('Authorization', 'Basic ' + auth.bytes.encodeBase64().toString())
-            }
-        }
+		this.requestInterceptor = new HttpRequestInterceptor() {
+					void process(HttpRequest httpRequest, HttpContext httpContext) {
+						def auth = jenkinsServerUser + ':' + jenkinsServerPassword
+						httpRequest.addHeader('Authorization', 'Basic ' + auth.bytes.encodeBase64().toString())
+					}
+				}
 
-        this.restClient.client.addRequestInterceptor(this.requestInterceptor)
-    }
+		this.restClient.client.addRequestInterceptor(this.requestInterceptor)
+	}
 
-    List<String> getJobNames(String prefix = null) {
-        println "getting project names from " + jenkinsServerUrl + "api/json"
-        def response = get(path: 'api/json')
-        def jobNames = response.data.jobs.name
-        if (prefix) return jobNames.findAll { it.startsWith(prefix) }
-        return jobNames
-    }
+	List<String> getJobNames(String prefix = null) {
+		println "getting project names from " + jenkinsServerUrl + "api/json"
+		def response = get(path: 'api/json')
+		def jobNames = response.data.jobs.name
+		if (prefix) return jobNames.findAll { it.startsWith(prefix) }
+		return jobNames
+	}
 
-    String getJobConfig(String jobName) {
-        def response = get(path: "job/${jobName}/config.xml", contentType: TEXT,
-                headers: [Accept: 'application/xml'])
-        response.data.text
-    }
+	String getJobConfig(String jobName) {
+		def response = get(path: "job/${jobName}/config.xml", contentType: TEXT,
+		headers: [Accept: 'application/xml'])
+		response.data.text
+	}
 
-    void cloneJobForBranch(String jobPrefix, ConcreteJob missingJob, String createJobInView) {
+	void cloneJobForBranch(String jobPrefix, ConcreteJob missingJob, String createJobInView) {
 		String createJobInViewPath = resolveViewPath(createJobInView)
 		println "-----> createInView after" + createJobInView
-        String missingJobConfig = configForMissingJob(missingJob)
-        TemplateJob templateJob = missingJob.templateJob
+		String missingJobConfig = configForMissingJob(missingJob)
+		TemplateJob templateJob = missingJob.templateJob
 
-        //Copy job with jenkins copy job api, this will make sure jenkins plugins get the call to make a copy if needed (promoted builds plugin needs this)
-        post(createJobInViewPath + 'createItem', missingJobConfig, [name: missingJob.jobName, mode: 'copy', from: templateJob.jobName], ContentType.XML)
+		//Copy job with jenkins copy job api, this will make sure jenkins plugins get the call to make a copy if needed (promoted builds plugin needs this)
+		post(createJobInViewPath + 'createItem', missingJobConfig, [name: missingJob.jobName, mode: 'copy', from: templateJob.jobName], ContentType.XML)
 
-        post('job/' + missingJob.jobName + "/config.xml", missingJobConfig, [:], ContentType.XML)
-        //Forced disable enable to work around Jenkins' automatic disabling of clones jobs
-        //But only if the original job was enabled
-        post('job/' + missingJob.jobName + '/disable')
-        if (!missingJobConfig.contains("<disabled>true</disabled>")) {
-            post('job/' + missingJob.jobName + '/enable')
-        }
-    }
-	
+		post('job/' + missingJob.jobName + "/config.xml", missingJobConfig, [:], ContentType.XML)
+		//Forced disable enable to work around Jenkins' automatic disabling of clones jobs
+		//But only if the original job was enabled
+		post('job/' + missingJob.jobName + '/disable')
+		if (!missingJobConfig.contains("<disabled>true</disabled>")) {
+			post('job/' + missingJob.jobName + '/enable')
+		}
+	}
+
 	public String resolveViewPath(String createInView) {
 		if (!createInView) {
 			return ""
@@ -78,126 +78,116 @@ class JenkinsApi {
 		elements.join();
 	}
 
-    void startJob(ConcreteJob job) {
-        println "Starting job ${job.jobName}."
-        post('job/' + job.jobName + '/build')
-    }
+	void startJob(ConcreteJob job) {
+		println "Starting job ${job.jobName}."
+		post('job/' + job.jobName + '/build')
+	}
 
-    String configForMissingJob(ConcreteJob missingJob) {
-        TemplateJob templateJob = missingJob.templateJob
-        String config = getJobConfig(templateJob.jobName)
+	String configForMissingJob(ConcreteJob missingJob) {
+		TemplateJob templateJob = missingJob.templateJob
+		String config = getJobConfig(templateJob.jobName)
+		return processConfig(config, missingJob.branchName)
+	}
 
-        def ignoreTags = ["assignedNode"]
+	public String processConfig(String entryConfig, String branchName) {
+		def xml = new XmlParser().parseText(entryConfig)
+		def branches = xml.scm.branches
+		xml.scm.branches."hudson.plugins.git.BranchSpec".name[0].value = "*/$branchName"
+		
+		def writer = new StringWriter()
+		new XmlNodePrinter(new PrintWriter(writer)).print(xml)
+		return writer.toString()
+	}
 
-        // should work if there's a remote ("origin/master") or no remote (just "master")
-        config = config.replaceAll("(\\p{Alnum}*[>/])(${templateJob.templateBranchName})<") { fullMatch, prefix, branchName ->
-            // jenkins job configs may have certain fields whose values should not be replaced, the most common being <assignedNode>
-            // which is used to assign a job to a specific node (potentially "master") and the "master" branch
-            if (ignoreTags.find { it + ">" == prefix}) {
-                return fullMatch
-            } else {
-                return "$prefix${missingJob.branchName}<"
-            }
-        }
+	void deleteJob(String jobName) {
+		println "deleting job $jobName"
+		post("job/${jobName}/doDelete")
+	}
 
-        // this is in case there are other down-stream jobs that this job calls, we want to be sure we're replacing their names as well
-		//TODO: Check if needed
-//        templateJobs.each {
-//            config = config.replaceAll(it.jobName, it.jobNameForBranch(missingJob.branchName))
-//        }
+	protected get(Map map) {
+		// get is destructive to the map, if there's an error we want the values around still
+		Map mapCopy = map.clone() as Map
+		def response
 
-        return config
-    }
+		assert mapCopy.path != null, "'path' is a required attribute for the GET method"
 
-    void deleteJob(String jobName) {
-        println "deleting job $jobName"
-        post("job/${jobName}/doDelete")
-    }
+		try {
+			response = restClient.get(map)
+		} catch (HttpHostConnectException ex) {
+			println "Unable to connect to host: $jenkinsServerUrl"
+			throw ex
+		} catch (UnknownHostException ex) {
+			println "Unknown host: $jenkinsServerUrl"
+			throw ex
+		} catch (HttpResponseException ex) {
+			def message = "Unexpected failure with path $jenkinsServerUrl${mapCopy.path}, HTTP Status Code: ${ex.response?.status}, full map: $mapCopy"
+			throw new Exception(message, ex)
+		}
 
-    protected get(Map map) {
-        // get is destructive to the map, if there's an error we want the values around still
-        Map mapCopy = map.clone() as Map
-        def response
+		assert response.status < 400
+		return response
+	}
 
-        assert mapCopy.path != null, "'path' is a required attribute for the GET method"
-
-        try {
-            response = restClient.get(map)
-        } catch (HttpHostConnectException ex) {
-            println "Unable to connect to host: $jenkinsServerUrl"
-            throw ex
-        } catch (UnknownHostException ex) {
-            println "Unknown host: $jenkinsServerUrl"
-            throw ex
-        } catch (HttpResponseException ex) {
-            def message = "Unexpected failure with path $jenkinsServerUrl${mapCopy.path}, HTTP Status Code: ${ex.response?.status}, full map: $mapCopy"
-            throw new Exception(message, ex)
-        }
-
-        assert response.status < 400
-        return response
-    }
-
-    /**
-     * @author Kelly Robinson
-     * from https://github.com/kellyrob99/Jenkins-api-tour/blob/master/src/main/groovy/org/kar/hudson/api/PostRequestSupport.groovy
-     */
-    protected Integer post(String path, postBody = [:], params = [:], ContentType contentType = ContentType.URLENC) {
+	/**
+	 * @author Kelly Robinson
+	 * from https://github.com/kellyrob99/Jenkins-api-tour/blob/master/src/main/groovy/org/kar/hudson/api/PostRequestSupport.groovy
+	 */
+	protected Integer post(String path, postBody = [:], params = [:], ContentType contentType = ContentType.URLENC) {
 
 		println "----> MAKING POST with PATH: " + path
-        //Added the support for jenkins CSRF option, this could be changed to be a build flag if needed.
-        //http://jenkinsurl.com/crumbIssuer/api/json  get crumb for csrf protection  json: {"crumb":"c8d8812d615292d4c0a79520bacfa7d8","crumbRequestField":".crumb"}
-        if (findCrumb) {
-            findCrumb = false
-            println "Trying to find crumb: ${jenkinsServerUrl}crumbIssuer/api/json"
-            try {
-                def response = restClient.get(path: "crumbIssuer/api/json")
+		//Added the support for jenkins CSRF option, this could be changed to be a build flag if needed.
+		//http://jenkinsurl.com/crumbIssuer/api/json  get crumb for csrf protection  json: {"crumb":"c8d8812d615292d4c0a79520bacfa7d8","crumbRequestField":".crumb"}
+		if (findCrumb) {
+			findCrumb = false
+			println "Trying to find crumb: ${jenkinsServerUrl}crumbIssuer/api/json"
+			try {
+				def response = restClient.get(path: "crumbIssuer/api/json")
 
-                if (response.data.crumbRequestField && response.data.crumb) {
-                    crumbInfo = [:]
-                    crumbInfo['field'] = response.data.crumbRequestField
-                    crumbInfo['crumb'] = response.data.crumb
-                }
-                else {
-                    println "Found crumbIssuer but didn't understand the response data trying to move on."
-                    println "Response data: " + response.data
-                }
-            }
-            catch (HttpResponseException e) {
-                if (e.response?.status == 404) {
-                    println "Couldn't find crumbIssuer for jenkins. Just moving on it may not be needed."
-                }
-                else {
-                    def msg = "Unexpected failure on ${jenkinsServerUrl}crumbIssuer/api/json: ${resp.statusLine} ${resp.status}"
-                    throw new Exception(msg)
-                }
-            }
-        }
+				if (response.data.crumbRequestField && response.data.crumb) {
+					crumbInfo = [:]
+					crumbInfo['field'] = response.data.crumbRequestField
+					crumbInfo['crumb'] = response.data.crumb
+				}
+				else {
+					println "Found crumbIssuer but didn't understand the response data trying to move on."
+					println "Response data: " + response.data
+				}
+			}
+			catch (HttpResponseException e) {
+				if (e.response?.status == 404) {
+					println "Couldn't find crumbIssuer for jenkins. Just moving on it may not be needed."
+				}
+				else {
+					def msg = "Unexpected failure on ${jenkinsServerUrl}crumbIssuer/api/json: ${resp.statusLine} ${resp.status}"
+					throw new Exception(msg)
+				}
+			}
+		}
 
-        if (crumbInfo) {
-            params[crumbInfo.field] = crumbInfo.crumb
-        }
+		if (crumbInfo) {
+			params[crumbInfo.field] = crumbInfo.crumb
+		}
 
-        HTTPBuilder http = new HTTPBuilder(jenkinsServerUrl)
+		HTTPBuilder http = new HTTPBuilder(jenkinsServerUrl)
 
-        if (requestInterceptor) {
-            http.client.addRequestInterceptor(this.requestInterceptor)
-        }
+		if (requestInterceptor) {
+			http.client.addRequestInterceptor(this.requestInterceptor)
+		}
 
-        Integer status = HttpStatus.SC_EXPECTATION_FAILED
+		Integer status = HttpStatus.SC_EXPECTATION_FAILED
 
-        http.handler.failure = { resp ->
-            def msg = "Unexpected failure on $jenkinsServerUrl$path: ${resp.statusLine} ${resp.status}"
-            status = resp.statusLine.statusCode
-            throw new Exception(msg)
-        }
+		http.handler.failure = { resp ->
+			def msg = "Unexpected failure on $jenkinsServerUrl$path: ${resp.statusLine} ${resp.status}"
+			status = resp.statusLine.statusCode
+			throw new Exception(msg)
+		}
 
-        http.post(path: path, body: postBody, query: params,
-                requestContentType: contentType) { resp ->
-            assert resp.statusLine.statusCode < 400
-            status = resp.statusLine.statusCode
-        }
-        return status
-    }
+		http.post(path: path, body: postBody, query: params,
+		requestContentType: contentType) { resp ->
+			assert resp.statusLine.statusCode < 400
+			status = resp.statusLine.statusCode
+		}
+		return status
+	}
 
 }
--- src/test/groovy/com/entagen/jenkins/JenkinsApiTests.groovy
+++ src/test/groovy/com/entagen/jenkins/JenkinsApiTests.groovy
@@ -13,117 +13,175 @@ import net.sf.json.JSONObject
 
 class JenkinsApiTests extends GroovyTestCase {
 
-    @Test public void testInvalidHostThrowsConnectionException() {
-        JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://some-invalid-hostname:9090/jenkins")
-        assert shouldFail(UnknownHostException) {
-            api.getJobNames("myproj")
-        }.contains("some-invalid-hostname")
-    }
+	@Test public void testInvalidHostThrowsConnectionException() {
+		JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://some-invalid-hostname:9090/jenkins")
+		assert shouldFail(UnknownHostException) { api.getJobNames("myproj") }.contains("some-invalid-hostname")
+	}
 
-    @Test public void testCantConnectToEndpointThrowsException() {
-        JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:12345/jenkins")
-        assert "Connection to http://localhost:12345 refused" == shouldFail(HttpHostConnectException) {
-            api.getJobNames("myproj")
-        }
+	@Test public void testCantConnectToEndpointThrowsException() {
+		JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:12345/jenkins")
+		assert "Connection to http://localhost:12345 refused" == shouldFail(HttpHostConnectException) { api.getJobNames("myproj") }
+	}
 
-    }
+	@Test public void test404ThrowsException() {
+		MockFor mockRESTClient = new MockFor(RESTClient)
+		mockRESTClient.demand.get { Map<String, ?> args ->
+			def ex = new HttpResponseException(404, "Not Found")
+			ex.metaClass.getResponse = {-> [status: 404] }
+			throw ex
+		}
 
-    @Test public void test404ThrowsException() {
-        MockFor mockRESTClient = new MockFor(RESTClient)
-        mockRESTClient.demand.get { Map<String, ?> args ->
-            def ex = new HttpResponseException(404, "Not Found")
-            ex.metaClass.getResponse = {-> [status: 404] }
-            throw ex
-        }
-
-        mockRESTClient.use {
-            JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:9090/goodHostAndPortBadUrl")
-            assert "Unexpected failure with path http://localhost:9090/goodHostAndPortBadUrl/api/json, HTTP Status Code: 404, full map: [path:api/json]" == shouldFail() {
-                api.getJobNames("myproj")
-            }
-        }
-    }
+		mockRESTClient.use {
+			JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:9090/goodHostAndPortBadUrl")
+			assert "Unexpected failure with path http://localhost:9090/goodHostAndPortBadUrl/api/json, HTTP Status Code: 404, full map: [path:api/json]" == shouldFail() { api.getJobNames("myproj") }
+		}
+	}
 
 	@Test public void testCreateInViewResolutor() {
 		JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:9090/jenkins")
 		assert api.resolveViewPath("abc/def") == "view/abc/view/def/"
 	}
-	
-    @Test public void testGetJobNames_matchPrefix() {
-        JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:9090/jenkins")
 
-        Map json = [
-                jobs: [
-                        [name: "myproj-FirstJob"],
-                        [name: "otherproj-SecondJob"]
-                ]
-        ]
-        withJsonResponse(json) {
-            List<String> projectNames = api.getJobNames("myproj")
-            assert projectNames == ["myproj-FirstJob"]
-        }
-    }
+	@Test public void testGetJobNames_matchPrefix() {
+		JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:9090/jenkins")
 
-    @Test public void testGetJobNames_noPrefix() {
-        JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:9090/jenkins")
+		Map json = [
+			jobs: [
+				[name: "myproj-FirstJob"],
+				[name: "otherproj-SecondJob"]
+			]
+		]
+		withJsonResponse(json) {
+			List<String> projectNames = api.getJobNames("myproj")
+			assert projectNames == ["myproj-FirstJob"]
+		}
+	}
 
-        Map json = [
-                jobs: [
-                        [name: "myproj-FirstJob"],
-                        [name: "otherproj-SecondJob"]
-                ]
-        ]
-        withJsonResponse(json) {
-            List<String> projectNames = api.jobNames
-            assert projectNames.sort() == ["myproj-FirstJob", "otherproj-SecondJob"]
-        }
-    }
+	@Test public void testGetJobNames_noPrefix() {
+		JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:9090/jenkins")
 
-    @Test public void testConfigForMissingJob_worksWithRemote() {
-        JenkinsApi api = new JenkinsApi()
-        api.metaClass.getJobConfig = { String jobName ->
-            "<name>origin/master</name>"
-        }
+		Map json = [
+			jobs: [
+				[name: "myproj-FirstJob"],
+				[name: "otherproj-SecondJob"]
+			]
+		]
+		withJsonResponse(json) {
+			List<String> projectNames = api.jobNames
+			assert projectNames.sort() == [
+				"myproj-FirstJob",
+				"otherproj-SecondJob"
+			]
+		}
+	}
 
-        TemplateJob templateJob = new TemplateJob(templateBranchName: "master")
-        ConcreteJob missingJob = new ConcreteJob(branchName: "new/branch", templateJob: templateJob)
-        assert "<name>origin/new/branch</name>" == api.configForMissingJob(missingJob, [])
-    }
+	@Test public void testConfigForMissingJob_worksWithRemote() {
+		JenkinsApi api = new JenkinsApi()
+		api.metaClass.getJobConfig = { String jobName -> "<name>origin/master</name>" }
 
-    @Test public void testConfigForMissingJob_worksWithoutRemote() {
-        JenkinsApi api = new JenkinsApi()
-        api.metaClass.getJobConfig = { String jobName ->
-            "<name>master</name>"
-        }
+		TemplateJob templateJob = new TemplateJob(templateBranchName: "master")
+		ConcreteJob missingJob = new ConcreteJob(branchName: "new/branch", templateJob: templateJob)
+		assert "<name>origin/new/branch</name>" == api.configForMissingJob(missingJob, [])
+	}
 
-        TemplateJob templateJob = new TemplateJob(templateBranchName: "master")
-        ConcreteJob missingJob = new ConcreteJob(branchName: "new/branch", templateJob: templateJob)
-        assert "<name>new/branch</name>" == api.configForMissingJob(missingJob, [])
-    }
+	@Test public void testConfigForMissingJob_worksWithoutRemote() {
+		JenkinsApi api = new JenkinsApi()
+		api.metaClass.getJobConfig = { String jobName -> "<name>master</name>" }
 
-    @Test public void testConfigForMissingJob_worksWithExclusions() {
-        JenkinsApi api = new JenkinsApi()
-        api.metaClass.getJobConfig = { String jobConfig ->
-            "<assignedNode>master</assignedNode>"
-        }
+		TemplateJob templateJob = new TemplateJob(templateBranchName: "master")
+		ConcreteJob missingJob = new ConcreteJob(branchName: "new/branch", templateJob: templateJob)
+		assert "<name>new/branch</name>" == api.configForMissingJob(missingJob, [])
+	}
 
-        TemplateJob templateJob = new TemplateJob(templateBranchName: "master")
-        ConcreteJob missingJob = new ConcreteJob(branchName: "new/branch", templateJob:  templateJob)
-        assert "<assignedNode>master</assignedNode>" == api.configForMissingJob(missingJob, [])
-    }
+	@Test public void testConfigForMissingJob_worksWithExclusions() {
+		JenkinsApi api = new JenkinsApi()
+		api.metaClass.getJobConfig = { String jobConfig -> "<assignedNode>master</assignedNode>" }
 
-    public void withJsonResponse(Map toJson, Closure closure) {
-        JSON json = toJson as JSONObject
-        MockFor mockRESTClient = new MockFor(RESTClient)
-        mockRESTClient.demand.get { Map<String, ?> args ->
-            return [data: json]
-        }
+		TemplateJob templateJob = new TemplateJob(templateBranchName: "master")
+		ConcreteJob missingJob = new ConcreteJob(branchName: "new/branch", templateJob:  templateJob)
+		assert "<assignedNode>master</assignedNode>" == api.configForMissingJob(missingJob, [])
+	}
 
-        mockRESTClient.use {
-            closure()
-        }
-    }
+	@Test public void testChangeBranchInConfig() {
+		String config = '''
+	<maven2-moduleset plugin="maven-plugin@2.7.1">
+	  <actions/>
+	  <description></description>
+	  <keepDependencies>false</keepDependencies>
+	  <properties/>
+	  <scm class="hudson.plugins.git.GitSCM" plugin="git@2.2.1">
+	    <configVersion>2</configVersion>
+	    <userRemoteConfigs>
+	      <hudson.plugins.git.UserRemoteConfig>
+	        <url>git@gitlab.neoteric.eu:developers/neob2b-neodocs.git</url>
+	        <credentialsId>469a31b3-b5e5-45e0-b9c6-9cc3ef61203e</credentialsId>
+	      </hudson.plugins.git.UserRemoteConfig>
+	    </userRemoteConfigs>
+	    <branches>
+	      <hudson.plugins.git.BranchSpec>
+	        <name>*/templates-feature</name>
+	      </hudson.plugins.git.BranchSpec>
+	    </branches>
+	    <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+	    <browser class="hudson.plugins.git.browser.GitLab">
+	      <url></url>
+	      <version>7.0</version>
+	    </browser>
+	    <submoduleCfg class="list"/>
+	    <extensions/>
+	  </scm>
+	  <canRoam>true</canRoam>
+	  <disabled>false</disabled>
+	  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+	  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+	  <triggers/>
+	  <concurrentBuild>false</concurrentBuild>
+	  <rootModule>
+	    <groupId>com.neoteric.b2b</groupId>
+	    <artifactId>neodocs</artifactId>
+	  </rootModule>
+	  <goals>clean install</goals>
+	  <aggregatorStyleBuild>true</aggregatorStyleBuild>
+	  <incrementalBuild>false</incrementalBuild>
+	  <ignoreUpstremChanges>true</ignoreUpstremChanges>
+	  <archivingDisabled>false</archivingDisabled>
+	  <siteArchivingDisabled>false</siteArchivingDisabled>
+	  <fingerprintingDisabled>false</fingerprintingDisabled>
+	  <resolveDependencies>false</resolveDependencies>
+	  <processPlugins>false</processPlugins>
+	  <mavenValidationLevel>-1</mavenValidationLevel>
+	  <runHeadless>false</runHeadless>
+	  <disableTriggerDownstreamProjects>false</disableTriggerDownstreamProjects>
+	  <blockTriggerWhenBuilding>true</blockTriggerWhenBuilding>
+	  <settings class="jenkins.mvn.DefaultSettingsProvider"/>
+	  <globalSettings class="jenkins.mvn.DefaultGlobalSettingsProvider"/>
+	  <reporters/>
+	  <publishers/>
+	  <buildWrappers/>
+	  <prebuilders/>
+	  <postbuilders/>
+	  <runPostStepsIfResult>
+	    <name>FAILURE</name>
+	    <ordinal>2</ordinal>
+	    <color>RED</color>
+	    <completeBuild>true</completeBuild>
+	  </runPostStepsIfResult>
+	</maven2-moduleset>
+	'''
+		
+	JenkinsApi api = new JenkinsApi(jenkinsServerUrl: "http://localhost:9090/jenkins")
+	def result = api.processConfig(config, "release-1.0.0");
+	println result
+	}
 
+	public void withJsonResponse(Map toJson, Closure closure) {
+		JSON json = toJson as JSONObject
+		MockFor mockRESTClient = new MockFor(RESTClient)
+		mockRESTClient.demand.get { Map<String, ?> args ->
+			return [data: json]
+		}
 
+		mockRESTClient.use { closure() }
+	}
 }
 
